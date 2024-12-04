fb6d9a394dad9a5e2d101f314ead3e38b6e6022e
ElieMaged <Narutossj23@yahoo.com>
2024-12-04 14:58:45 +0200

    main

--- app/_components/Card.tsx
+++ app/_components/Card.tsx
@@ -9,18 +9,17 @@ export default function Card({productList = []}:any) {
          
             <div key={item.id} className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
                 <a href="#">
-                    <Image 
-                    src={Pic}
-                    width={500}
-                    height={500}
+                    <img 
+                    src={`http://localhost:1337${item.url.url}`}
                     alt='keyboard'
+                    className='w-100% h-40'
                     />
                 </a>
                 {item.title}
                 {console.log(item)}
                 <div className="p-5">
                     <a href="#">
-                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{item.description[0]?.children[0]?.text}</h5>
+                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"></h5>
                     </a>
                     <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{item.desc}</p>
                     <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
