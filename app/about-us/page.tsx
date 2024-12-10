28d126fc0cfb719cc9fcaef7e386b0e943359ed8
ElieMaged <Narutossj23@yahoo.com>
2024-12-10 20:19:43 +0200

    r

---
+++ app/about-us/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function AboutUs() {
+  return (
+    <div className='m-5'>
+    <div className='grid text-black grid-cols-2 m-5'>
+        <span className='cols-span-1'>
+      <h1 className='4xl'>Our Story</h1>
+      <p>Launced in 2015, Exclusive is South Asia’s premier online shopping makterplace with an active presense in Bangladesh. Supported by wide range of tailored marketing, data and service solutions, Exclusive has 10,500 sallers and 300 brands and serves 3 millioons customers across the region. </p>
+      </span>
+      <img className='cols-span-1' src="https://media.istockphoto.com/id/1446098147/photo/shopping-happy-and-portrait-of-customer-with-bag-after-shopping-spree-buying-retail-fashion.jpg?s=612x612&w=0&k=20&c=OFL3OPm9SPaaUveM_z3-0DaFcvorK4H_wK4KnmSY0Og=" alt="" />
+    </div>
+    </div>
+  )
+}
+
+export default AboutUs
